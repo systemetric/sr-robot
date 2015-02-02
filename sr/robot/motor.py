f75ac5a778aa120ea2a77a4a55f5b8843fc6cc7b
Tom Leese <inbox@tomleese.me.uk>
2015-02-02 18:34:06 +0000

    Remove unused code
    This is a remnant from when 'usbenum' was imported. It's not actually
    called from anywhere, instead the power board code enumerates USB
    devices.
    
    Patch Set #2: Remove removable of 'import usbenum' line. This has been
                  fixed upstream.
    Patch Set #3: Fix typo in commit message.
    
    Change-Id: I84b05545b85f633eb560c6a14a485e4cd8c42b04

--- sr/robot/motor.py
+++ sr/robot/motor.py
@@ -20,24 +20,6 @@ USB_MODEL = "MCV4B"
 
 logger = logging.getLogger( "sr.motor" )
 
-def list_usb_devices(model):
-    "Create a sorted list of USB devices of the given type"
-    def _udev_compare_serial(x, y):
-        """Compare two udev serial numbers"""
-        return cmp(x["ID_SERIAL_SHORT"],
-                   y["ID_SERIAL_SHORT"])
-
-    udev = pyudev.Context()
-    devs = list(udev.list_devices( subsystem = "tty",
-                                   ID_MODEL = model ))
-    # Sort by serial number
-    devs.sort( cmp = _udev_compare_serial )
-    return devs
-
-
-def find_devs():
-    return list_usb_devices(USB_MODEL)
-
 class IncorrectFirmware(Exception):
     pass
 
