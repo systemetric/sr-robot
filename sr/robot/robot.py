a0e8347c25e9693640481ed1b638dfb5434d3ba2
Josh Holland <anowlcalledjosh@gmail.com>
2017-10-12 12:11:57 +0100

    Convert sorting from using cmp to using key

--- sr/robot/robot.py
+++ sr/robot/robot.py
@@ -265,15 +265,10 @@ class Robot(object):
 
     def _list_usb_devices(self, model, subsystem=None):
         """Create a sorted list of USB devices of the given type"""
-        def _udev_compare_serial(x, y):
-            """Compare two udev serial numbers"""
-            return cmp(x["ID_SERIAL_SHORT"],
-                       y["ID_SERIAL_SHORT"])
-
         udev = pyudev.Context()
         devs = list(udev.list_devices( ID_MODEL = model, subsystem=subsystem ))
         # Sort by serial number
-        devs.sort( cmp = _udev_compare_serial )
+        devs.sort(key=lambda x: x["ID_SERIAL_SHORT"])
         return devs
 
     def _init_usb_devices(self, model, ctor, subsystem=None):
